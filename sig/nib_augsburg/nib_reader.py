e1216783a7a74155096b8a956a7e562476a5db51
Clément Besnier <clemsciences@aol.com>
2019-09-29 21:22:55 +0200

    Added TEI reader of Nibelungenlied

--- sig/nib_augsburg/nib_reader.py
+++ sig/nib_augsburg/nib_reader.py
@@ -6,16 +6,23 @@ import os
 import codecs
 from typing import List
 
+from lxml import etree
 from bs4 import BeautifulSoup
+
+from sig.nib_augsburg.nib_retrieval import MAIN_LINKS
 
 __author__ = ["Clément Besnier <clemsciences@aol.com>", ]
 
 nib_directories = ["nib_a_00", "nib_b_00", "nib_c_00", "nib_n_00"]
 
+NAMESPACES = {'n': 'http://www.tei-c.org/ns/1.0'}
+
 
 def extract_text_from_html(main_links: List[str]) -> dict:
     """
     From HTML files to TXT files
+
+    >>> extract_text_from_html(MAIN_LINKS)
 
     :param main_links:
     :return:
@@ -37,6 +44,8 @@ def read_txt(main_link: str) -> List:
     """
     TXT files to str
 
+    >>> read_txt(MAIN_LINKS[0])
+
     :param main_link:
     :return:
     """
@@ -49,3 +58,33 @@ def read_txt(main_link: str) -> List:
             lines = [line.split("\t") for line in text.split("\n")]
         retrieved_texts.append(lines)
     return retrieved_texts
+
+
+def get_xml_root(main_link):
+    """
+    >>> get_xml_root(MAIN_LINKS[0])
+
+    :param main_link:
+    :return:
+    """
+    parser = etree.XMLParser(load_dtd=True, no_network=False)
+    filename = main_link.split("/")[-1].split(".")[0][:-3]+".xml"
+    tree = etree.parse(filename, parser=parser)
+    return tree.getroot()
+
+
+def read_tei(main_link):
+    """
+    >>> read_tei(MAIN_LINKS[0])[0][0]
+    ['UNS IST> In alten', 'mæren wnders vil geseit']
+
+    :param main_link:
+    :return:
+    """
+    root = get_xml_root(main_link)
+    chapters = [node for node in root.findall(".//n:chapter", namespaces=NAMESPACES)]
+    # lines = [[line.text for line in chapter.findall(".//l")] for chapter in chapters]
+    segments = [[[segment.text for segment in line.findall(".//n:seg", namespaces=NAMESPACES)]
+                 for line in chapter.findall(".//n:l", namespaces=NAMESPACES)]
+                for chapter in chapters]
+    return segments

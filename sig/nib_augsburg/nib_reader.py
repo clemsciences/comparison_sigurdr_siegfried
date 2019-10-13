be22b0b341fc798a1bc17d49709c7c5eaadfdfcc
Clément Besnier <clemsciences@aol.com>
2019-10-14 00:23:15 +0200

    Added places and names readers

--- sig/nib_augsburg/nib_reader.py
+++ sig/nib_augsburg/nib_reader.py
@@ -89,3 +89,20 @@ def read_tei(main_link):
                  for line in chapter.findall(".//n:l", namespaces=NAMESPACES)]
                 for chapter in chapters]
     return segments
+
+
+def read_places():
+    with codecs.open(os.path.join(PACKDIR, "nib_augsburg", "annotations", "places.txt"), encoding="utf-8") as f:
+        lines = f.read().strip().split(os.linesep)
+
+    places = [line.split(":") for line in lines if len(line) > 0 and not line.startswith("#")]
+    places = {place[0]: [p for p in place[1].split(" ") if p] for place in places if len(place) == 2}
+    return places
+
+
+def read_names():
+    with codecs.open(os.path.join(PACKDIR, "nib_augsburg", "annotations", "names.txt"), encoding="utf-8") as f:
+        lines = f.read().strip().split(os.linesep)
+    names = [line.split(":") for line in lines if len(line) > 0 and not line.startswith("#")]
+    names = {name[0]: [n.strip() for n in name[1].split(" ") if n] for name in names if len(name) == 2}
+    return names

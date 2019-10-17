7e50dea61f91049471a7903616ee192ca30a511c
clement_besnier <clemsciences@aol.com>
2019-10-17 18:21:37 +0200

    Associates normalized token, lemma and pos tag

--- sig/nib_marburg/rem_reader.py
+++ sig/nib_marburg/rem_reader.py
@@ -30,8 +30,16 @@ def read_text(filename):
     transcriptions_anno = []
     transcriptions_anno_utf = []
 
-    normalised = [[]]
-    lemmatized = [[]]
+    annotations = [[]]
+
+    # normalised = [[]]
+    # lemmatized = [[]]
+    # pos = [[]]
+    # pos_gen = [[]]
+    # infl = [[]]
+    # infl_class = [[]]
+    # infl_class_gen = [[]]
+
     i = 0
 
     for entry in entries:
@@ -43,14 +51,53 @@ def read_text(filename):
         if entry.get("type") == "token":
             child_norm = child_anno.find("norm")
             child_lemma = child_anno.find("lemma")
-            normalised[i].append(child_norm.get("tag"))
-            lemmatized[i].append(child_lemma.get("tag"))
-        else:
-            normalised[i].append(child_diplo.get("utf"))
-            normalised.append([])
+            child_pos = child_anno.find("pos")
+            child_pos_gen = child_anno.find("pos_gen")
+            child_infl = child_anno.find("infl")
+            child_infl_class = child_anno.find("inflClass")
+            child_infl_class_gen = child_anno.find("inflClass_gen")
 
-            lemmatized[i].append(child_diplo.get("utf"))
-            lemmatized.append([])
+            # normalised[i].append(child_norm.get("tag"))
+            # lemmatized[i].append(child_lemma.get("tag"))
+            # pos[i].append(child_pos.get("tag"))
+            # pos_gen[i].append(child_pos_gen.get("tag"))
+            # infl[i].append(child_infl.get("tag"))
+            # infl_class[i].append(child_infl_class.get("tag"))
+            # infl_class_gen[i].append(child_infl_class_gen.get("tag"))
+
+            annotations[i].append((
+                child_norm.get("tag"),
+                child_lemma.get("tag"),
+                child_pos.get("tag"),
+                child_pos_gen.get("tag"),
+                child_infl.get("tag"),
+                child_infl_class.get("tag"),
+                child_infl_class_gen.get("tag")
+            ))
+        else:
+            # normalised[i].append(child_diplo.get("utf"))
+            # normalised.append([])
+            #
+            # lemmatized[i].append(child_diplo.get("utf"))
+            # lemmatized.append([])
+            #
+            # pos[i].append(child_diplo.get("utf"))
+            # pos.append([])
+            #
+            # pos_gen[i].append(child_diplo.get("utf"))
+            # pos_gen.append([])
+            #
+            # infl[i].append(child_diplo.get("utf"))
+            # infl.append([])
+            #
+            # infl_class[i].append(child_diplo.get("utf"))
+            # infl_class.append([])
+            #
+            # infl_class_gen[i].append(child_diplo.get("utf"))
+            # infl_class_gen.append([])
+
+            annotations[i].append(child_diplo.get("utf"))
+            annotations.append([])
 
             i += 1
 
@@ -58,7 +105,19 @@ def read_text(filename):
     # print(norms[:20])
     # with codecs.open(os.path.join(PREPROCESSED_DIRECTORY, "M036-N1.xml"), "r", encoding="utf-8") as f:
     #     f.write(norms)
-    print(normalised[:20])
+    # print(normalised[:20])
+    # print(pos[:20])
+    # print(pos_gen[:20])
+    # print(infl[:20])
+    # print(infl_class[:20])
+    # print(infl_class_gen[:20])
+
+    # return zip(normalised, pos, pos_gen, infl, infl_class, infl_class_gen)
+    return annotations
 
 
-read_text("M035-N1.xml")
+def annotation_text(filename):
+    pass
+
+
+print(list(read_text("M035-N1.xml"))[:10])

97e6b4402a32aeec2ae3ce08faa6d74eb43fe734
clement_besnier <clemsciences@aol.com>
2020-07-22 12:37:09 +0200

    Version 1.2.8

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
 
 setup(
     name="sigurd",
-    version="1.2.7",
+    version="1.2.8",
     author="Clément Besnier",
     author_email="clem@clementbesnier.fr",
     description="Code for presentation at Graz, 6th November 2019",
